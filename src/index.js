17e5f9171f6f9f800b7b42b3c374116a1ba29008
Charith <rajithacharith@gmail.com>
2018-08-23 22:53:08 +0530

    rendering a component

--- src/index.js
+++ src/index.js
@@ -9,42 +9,37 @@ const user = {
     firstName: 'Harper',
     lastName: 'Perez',
     avatarUrl: 'E:/Photography/Lightroom Exports/My Fav/1.jpg'
-  };
+};
   
-  const element = React.createElement(
+const elementOld = React.createElement(
     'h2',
     {className: 'greeting'},
     'Hello, world!'
-  );
+);
 
+const element = <Welcome name="Sara" />;
 
 
 //functions
 function formatName(user) {
     return user.firstName + ' ' + user.lastName;
-  }
+}
 
 function getGreeting(user) {
     if (user) {
       return <h1>Hello, {formatName(user)}!</h1>;
     }
     return <h1>Hello, Stranger.</h1>;
-  }
+}
 
   
 
-function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
-  }
   
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
 
 //main code
-  setInterval(tick, 1000);
+ReactDOM.render(element, document.getElementById('root'));
 //ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
