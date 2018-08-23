120bb500ef3a41b3b64bc6d782deea31eb7b5d3a
Charith <rajithacharith@gmail.com>
2018-08-23 21:43:05 +0530

    JSX 2

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const name = 'Charith Rajitha';
-const element = <h1>Hello, {name}</h1>;
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+  
+  const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+  };
+  
+  const element = (
+    <h1>
+      Hello, {formatName(user)}!
+    </h1>
+  );
 
 ReactDOM.render(
   element,
