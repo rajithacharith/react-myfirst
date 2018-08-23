811c8050a15f35e2eeb137b9d4a19b29218fefc1
Charith <rajithacharith@gmail.com>
2018-08-23 21:46:39 +0530

    added get greeting fn

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import registerServiceWorker from './registerServiceWorker';
 function formatName(user) {
     return user.firstName + ' ' + user.lastName;
   }
+
+function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+  }
   
   const user = {
     firstName: 'Harper',
