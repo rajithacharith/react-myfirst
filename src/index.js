a264337c1d98998e6d692573f255bfe7dc8d6828
Charith <rajithacharith@gmail.com>
2018-08-23 22:54:58 +0530

    composing componetns

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 //constants
@@ -39,7 +39,20 @@ function Welcome(props) {
     return <h1>Hello, {props.name}</h1>;
 }
 
+function App() {
+    return (
+      <div>
+        <Welcome name="Sara" />
+        <Welcome name="Cahal" />
+        <Welcome name="Edite" />
+      </div>
+    );
+  }
+
 //main code
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
 //ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
