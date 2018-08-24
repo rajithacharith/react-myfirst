4aa3c69d5c7aa29ecd210fa5ab08719076d88081
Charith <rajithacharith@gmail.com>
2018-08-25 04:32:30 +0530

    Greeting - conditional rendering

--- src/index.js
+++ src/index.js
@@ -78,6 +78,24 @@ function ActionLink(){
     );
 }
 
+function UserGreeting(props){
+    return <h1>Welcome Back!</h1>;
+}
+
+function GuestGreeting(props){
+    return <h1>Please Sign Up!</h1>
+}
+
+function Greeting(props){
+    const isLoggedIn = props.isLoggedIn;
+    if(isLoggedIn){
+        return <UserGreeting />;
+    }
+    return <GuestGreeting/>;
+}
+
+
+
 //Classes
 class Clock extends React.Component{
     constructor(props){
