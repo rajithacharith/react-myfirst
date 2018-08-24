365115f6b2bd2bc658135a1f46f4aee4fd389384
Charith <rajithacharith@gmail.com>
2018-08-25 04:38:19 +0530

    Login

--- src/index.js
+++ src/index.js
@@ -94,7 +94,21 @@ function Greeting(props){
     return <GuestGreeting/>;
 }
 
-
+function LoginButton(props) {
+    return (
+      <button onClick={props.onClick}>
+        Login
+      </button>
+    );
+}
+  
+function LogoutButton(props) {
+    return (
+      <button onClick={props.onClick}>
+        Logout
+      </button>
+    );
+}
 
 //Classes
 class Clock extends React.Component{
@@ -167,9 +181,47 @@ class LoggingButton extends React.Component{
     }
 }
 
+
+class LoginControl extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleLoginClick = this.handleLoginClick.bind(this);
+      this.handleLogoutClick = this.handleLogoutClick.bind(this);
+      this.state = {isLoggedIn: false};
+    }
+  
+    handleLoginClick() {
+      this.setState({isLoggedIn: true});
+    }
+  
+    handleLogoutClick() {
+      this.setState({isLoggedIn: false});
+    }
+  
+    render() {
+      const isLoggedIn = this.state.isLoggedIn;
+      let button;
+  
+      if (isLoggedIn) {
+        button = <LogoutButton onClick={this.handleLogoutClick} />;
+      } else {
+        button = <LoginButton onClick={this.handleLoginClick} />
+      }
+  
+      return (
+        <div>
+          <Greeting isLoggedIn={isLoggedIn} />
+          {button}
+        </div>
+      );
+    }
+}
+
+
+
 //main code
 ReactDOM.render(
-    <LoggingButton />,
+    <LoginControl />,
     document.getElementById('root')
 );
 
