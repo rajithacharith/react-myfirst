4b31f5e1e76b24ae6b4287affd548002e5c8669e
Charith <rajithacharith@gmail.com>
2018-08-24 19:01:06 +0530

    add loggin button - woriking fine

--- src/index.js
+++ src/index.js
@@ -135,9 +135,23 @@ class Toggle extends React.Component{
     }
 }
 
+class LoggingButton extends React.Component{
+    handleClick = () => {
+        console.log('this is:', this);
+    }
+
+    render(){
+        return(
+            <button onClick = {this.handleClick}>
+            Click me
+            </button>
+        );
+    }
+}
+
 //main code
 ReactDOM.render(
-    <Toggle />,
+    <LoggingButton />,
     document.getElementById('root')
 );
 
